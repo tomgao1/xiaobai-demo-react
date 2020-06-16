bd774871cbaddfb282aa97a185d6a66f3c55b8d6
tomgao <406038564@qq.com>
2020-06-16 16:13:09 +0800

    完成导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,94 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 
 
-
-
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+const Wrapper = styled.div`
+  border: 1px solid red;
+  min-height:100vh;
+  display: flex;
+  flex-direction:column;
+`
+const Main = styled.div`
+border: 1px solid green;
+flex-grow:1;
+overflow:auto;
+`
+const Nav = styled.nav`
+ border: 1px solid blue;
+ > ul{
+   display: flex;
+    > li{
+      width:33.333%;
+      text-align:center;
+      padding: 16px;
+     }
+    }
+`;
 function App() {
   return (
-   <div>hello
-
-   </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
 }
+
+function NoMatch() {
+  return (
+    <div>页面不存在，你输错地址了！</div>
+  );
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+ 
+
 
 export default App;
