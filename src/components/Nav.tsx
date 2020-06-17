7703297255e1bf16aca48f95ba161a6d34ca76d7
tomgao <406038564@qq.com>
2020-06-17 10:04:15 +0800

    搞定icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 
+require('icons/money.svg');
+require('icons/tag.svg');
+require('icons/chart.svg');
+
 const NavWrapper = styled.nav`
  line-height: 24px;
  box-shadow: 0 0 3px rgba(0,0,0,0.25);
-
+ 
   > ul {
    display: flex;
     > li {
       width:33.333%;
       text-align:center;
-      padding: 16px;
+      display: flex;
+      flex-direction: column;
+      padding: 4px 0;
+      justify-content: center;
+      align-items: center;
+      .icon {
+        width: 24px;      
+        height: 24px;
+      }
      }
     }
 `;
@@ -21,12 +33,21 @@ const Nav = ()=> {
         <NavWrapper>  
      <ul>
    <li>
+   <svg className="icon">
+            <use xlinkHref="#tag"/>
+          </svg>
     <Link to="/tags">标签页</Link>
   </li>
   <li>
+  <svg className="icon">
+            <use xlinkHref="#money"/>
+          </svg>
     <Link to="/money">记账页</Link>
   </li>
   <li>
+  <svg className="icon">
+            <use xlinkHref="#chart"/>
+          </svg>
     <Link to="/statistics">统计页</Link>
   </li>
 </ul>
