7a331970981dce8c98f980522f9dc63a7e0c6327
tomgao <406038564@qq.com>
2020-06-17 12:18:08 +0800

    穿件了layout组件和views目录

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,35 @@
 import React from 'react';
-import styled from 'styled-components';
-import Nav from './components/Nav';
+import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
+import NoMatch from './views/NoMatch';
 
 
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-} from "react-router-dom";
 
-const Wrapper = styled.div`
-  border: 1px solid red;
-  min-height:100vh;
-  display: flex;
-  flex-direction:column;
-`
-const Main = styled.div`
-border: 1px solid green;
-flex-grow:1;
-overflow:auto;
-`
+
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Redirect exact from="/" to="/money"/>
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
-        </Main>
-       <Nav/>
-      </Wrapper>
+    <Switch>
+      <Route path="/tags">
+        <Tags/>
+      </Route>
+      <Route path="/money">
+        <Money/>
+      </Route>
+      <Route path="/statistics">
+        <Statistics/>
+      </Route>
+      <Redirect exact from="/" to="/money"/>
+        <Route path="*">
+          <NoMatch/>
+        </Route>
+    </Switch>
     </Router>
   );
 }
-
-function NoMatch() {
-  return (
-    <div>页面不存在，你输错地址了！</div>
-  );
-}
-
-function Statistics() {
-  return <h2>统计页面</h2>;
-}
-
-function Tags() {
-  return <h2>标签页面</h2>;
-}
-
-function Money() {
-  return <h2>记账页面</h2>;
-}
-
- 
 
 
 export default App;
---
+++ src/views/NoMatch.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NoMatch() {
+  return (
+    <div>页面不存在，你输错地址了</div>
+  );
+}
+
+export default NoMatch;
