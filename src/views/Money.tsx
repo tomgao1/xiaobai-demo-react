9b841faf20a80a0bc095cd5fcc3f9fe992322cf4
tomgao <406038564@qq.com>
2020-06-17 17:36:31 +0800

    Money.tsx之备注UI完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -18,6 +18,21 @@ background:#FFFFFF;padding: 12px 16px;
   }
 `
 const NotesSection = styled.section`
+background: #f5f5f5;
+padding: 0 16px;
+font-size: 14px;
+ > label {
+   display:flex;
+   align-items:center;
+   > span {margin-right:16px;white-space:nowrap;}
+   > input {
+     display: block;
+     width:100%;
+     height:72px;
+     background:none;
+     border:none;
+   }
+ }
 `;
 const CategorySection = styled.section`
 `;
@@ -44,7 +59,7 @@ function Money() {
      <NotesSection>
        <label>
          <span>备注</span>
-           <input type="text"/>
+           <input type="text" placeholder="在这里添加备注"/>
        </label>
      </NotesSection>
      <CategorySection>
