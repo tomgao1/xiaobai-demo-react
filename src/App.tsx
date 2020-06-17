7fb2b689a78dddd1751dca2a1393e924b992b35e
tomgao <406038564@qq.com>
2020-06-17 17:13:35 +0800

    Money.tsx之TagsSection UI完成

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import Money from './views/Money';
 import Statistics from './views/Statistics';
 import Tags from './views/Tags';
 import NoMatch from './views/NoMatch';
+import styled from 'styled-components';
 
+const AppWrapper = styled.div`
+color:#333;
+`;
 
 
 
 
 function App() {
   return (
+    <AppWrapper>
     <Router>
     <Switch>
       <Route path="/tags">
@@ -28,6 +33,7 @@ function App() {
         </Route>
     </Switch>
     </Router>
+    </AppWrapper>
   );
 }
 
