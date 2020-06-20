ed63fe11d453da079a0ccb3cd41541be5f45325a
tomgao <406038564@qq.com>
2020-06-20 15:48:44 +0800

    创建create.让id自增；封装findTag到useTags里

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import React from "react";
 import { useTags } from "useTags";
+import { createId } from "lib/createId";
 
 const Wrapper = styled.section`
 background:#FFFFFF;padding: 12px 16px;
@@ -24,37 +25,39 @@ background:#FFFFFF;padding: 12px 16px;
 `;
  
 type Props = {
-  value:string[];
-  onChange:(selected:string[]) =>void;
+  value:number[];
+  onChange:(selected:number[]) =>void;
 }
 
-const TagsSection: React.FC<Props> = (props) => {
- const{tags,setTags} = useTags();
-  const selectedTags = props.value;
+ const TagsSection: React.FC<Props> = (props) => {
+  const{tags,setTags} = useTags();
+  const selectedTagIds = props.value;
   const onAddTag = () =>{
     const tagName = window.prompt('新标签名称为');
     if (tagName !== null ) {
-      setTags([...tags,tagName]);
+      setTags([...tags,{id:createId(),name:tagName}]);
     }
   };
-   const onToggleTag = (tag: string) => {
-     const index = selectedTags.indexOf(tag);
+   const onToggleTag = (tagId: number) => {
+     const index = selectedTagIds.indexOf(tagId);
      if(index >=0) {
-        // 如果 tag 已被选中，就复制所有没有被选中的 tag，作为新的 selectedTag
-       props.onChange(selectedTags.filter(t=>t !==tag));
+       // 如果 tag 已被选中，就复制所有没有被选中的 tag，作为新的 selectedTag
+       props.onChange(selectedTagIds.filter(t=>t !==tagId));
      } else{
-      props.onChange([...selectedTags,tag])
+      props.onChange([...selectedTagIds,tagId])
      }
    };
-   const getClass = (tag:string) => selectedTags.indexOf(tag) >=0 ? 'selected' : '';
+   const getClass = (tagId:number) => selectedTagIds.indexOf(tagId) >=0 ? 'selected' : '';
    return (
      <Wrapper>
        <ol>
-         {tags.map(tag =>
-          <li key={tag} onClick={() =>{onToggleTag(tag);}
-          } className={getClass(tag)}>{tag}
-          </li>)}
-       </ol>
+        {tags.map(tag =>
+          <li key={tag.id} onClick={
+            () => {onToggleTag(tag.id);}
+          } className={getClass(tag.id)}
+          >{tag.name}</li>
+        )}
+      </ol>
        <button onClick={onAddTag}>新增标签</button>
      </Wrapper>
    )
