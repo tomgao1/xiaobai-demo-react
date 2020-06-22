042caa87c2e02968267d1da209280aee7f2cd9cc
tomgao <406038564@qq.com>
2020-06-22 17:45:32 +0800

    解决无法输入点的BUG

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,5 +1,5 @@
 import Layout from '../components/Layout';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import {TagsSection} from './Money/TagsSection';
 import {CategorySection} from './Money/CategorySection';
@@ -34,6 +34,12 @@ function Money() {
     setSelected(defaultFormData);
     }
   };
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('时间到');
+      setSelected({...selected, amount: 1000});
+    }, 3000);
+  }, []);
   return (
     <MyLayout>
     
